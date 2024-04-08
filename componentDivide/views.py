88a88f958e98c3e4233a8760458d9cfedaa4bb6e
NandaniThakur <nandaniharimira@gmail.com>
2024-04-08 09:45:44 +0530

    adding existing project files to git

---
+++ componentDivide/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+
+def index(request):
+    return render(request, 'index.html')
+
+def base(request):
+    return render(request, 'base.html')
+
+def basechild(request):
+    return render(request, 'basechild.html')
